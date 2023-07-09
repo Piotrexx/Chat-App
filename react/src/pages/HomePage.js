9bd00cc89e072c0fe7b21d19333000a76691746d
Peter <piotr.lopatecki@gmail.com>
2023-07-09 14:31:02 +0200

    the rooms chat are displayed at homepage

--- react/src/pages/HomePage.js
+++ react/src/pages/HomePage.js
@@ -1,8 +1,15 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
 function HomePage() {
     let {user, logoutUser} = useContext(AuthContext)
+    let [data, setData] = useState([])
+    useEffect(() => {
+      fetch('http://localhost:8000/chatroomform/')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error))
+    }, [])
     return (
       <div className=''>
           <Link to='/'>Home</Link>
@@ -10,9 +17,11 @@ function HomePage() {
           
           {user ? (
             <div>
+              <div>
               <Link onClick={logoutUser}>Logout</Link>
               <span> | </span>
               <Link to='/chatroomcreator'>Create new room</Link>
+              </div>
             </div>
 
           ): (
@@ -23,6 +32,27 @@ function HomePage() {
   
           {user && <p>Hello {user.username}</p>}
           
+
+
+          
+          {
+                data.map((item, index)=>(
+                  <ul key={index}>
+                    <p>Title: </p><li>
+                      {item.title}
+                    </li>
+                    <p>Description: </p><li>
+                      {item.description}
+                    </li>
+                    <p>Id of the author(will be changed to the username): </p><li>
+                      {item.author}
+                    </li>
+                    <br></br>
+                  </ul>
+
+                ))
+              }
+
       </div>
     )
 }
