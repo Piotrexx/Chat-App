7ec81635bcffa0edeb446ad8e800b17844cb343f
Peter <piotr.lopatecki@gmail.com>
2023-07-10 14:42:32 +0200

    made a little change to the API

--- backend/userapp/models.py
+++ backend/userapp/models.py
@@ -7,9 +7,13 @@ class ChatRoom(models.Model):
     description = models.CharField(max_length=250)
     private = models.BooleanField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-
+    
+    def __str__(self):
+        return self.title
+    
 class Message(models.Model):
     content = models.CharField(max_length=300)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     date_created = models.DateTimeField(auto_now_add=True)
     chatroom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, default=None)
+
