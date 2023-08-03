c0474203230c357a5ec03906ad0c1ee147d7669e
Peter <piotr.lopatecki@gmail.com>
2023-08-03 22:37:20 +0200

    made some improvements

--- react/src/pages/HomePage.js
+++ react/src/pages/HomePage.js
@@ -1,169 +1,180 @@
-import React, {useContext, useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
-import AuthContext from '../context/AuthContext'
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 function HomePage() {
-    let {user, logoutUser, acceptRequest, declineRequest} = useContext(AuthContext)
-    let [data, setData] = useState([])
-    let [username, setUsername] = useState([])
-    let [searchInput, setSearchInput] = useState('')
-    let  [filteredResults, setFilteredResults] = useState([])
-    let [friendRequest, setFriendRequest] = useState([])
-    let [friends, setFriends] = useState([])
-    let [converted, setConvert] = useState([])
-    let update
+  let { user, logoutUser, acceptRequest, declineRequest } =
+    useContext(AuthContext);
+  let [data, setData] = useState([]);
+  let [username, setUsername] = useState([]);
+  let [searchInput, setSearchInput] = useState("");
+  let [filteredResults, setFilteredResults] = useState([]);
+  let [friendRequest, setFriendRequest] = useState([]);
+  let [friends, setFriends] = useState([]);
+  let [converted, setConvert] = useState([]);
+  let update;
 
-  useEffect(() =>{
+  useEffect(() => {
     fetch(`http://127.0.0.1:8000/friendrequest/${user.user_id}/`)
-    .then(response => response.json())
-    .then(friendRequest => setFriendRequest(friendRequest))
-    .catch(error => console.log(error))
-  }, [])
+      .then((response) => response.json())
+      .then((friendRequest) => setFriendRequest(friendRequest))
+      .catch((error) => console.log(error));
+  }, []);
 
-  
-
-  useEffect(() =>{
+  useEffect(() => {
     fetch(`http://127.0.0.1:8000/friends/${user.user_id}/`)
-    .then(response => response.json())
-    .then(friends => setFriends(friends))
-    .catch(error => console.log(error))
-  }, [])
+      .then((response) => response.json())
+      .then((friends) => setFriends(friends))
+      .catch((error) => console.log(error));
+  }, []);
 
-    useEffect(() => {
-      fetch('http://localhost:8000/chatroomform/')
-      .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.log(error))
+  useEffect(() => {
+    fetch("http://localhost:8000/chatroomform/")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  }, []);
 
-    }, [])
+  useEffect(() => {
+    fetch("http://localhost:8000/user/")
+      .then((response) => response.json())
+      .then((username) => setUsername(username))
+      .catch((error) => console.log(error));
+  }, []);
 
-      useEffect(() => {
-        fetch('http://localhost:8000/user/')
-        .then(response => response.json())
-        .then(username => setUsername(username))
-        .catch(error => console.log(error))
-      }, [])
+  useEffect(() => {
+    fetch("http://localhost:8000/user/")
+      .then((response) => response.json())
+      .then((testing) => setConvert(testing[testing.filter]));
+  });
 
+  // console.log(username)
+  let SearchItems = (searchValue, friends) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      let filteredData = username.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      console.log('aojdhaw')
+    }
+  };
 
+  let HandleRequest = async (receiver) => {
+    let response = await fetch("http://127.0.0.1:8000/postingRequests/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        status: "pending",
+        sender: user.user_id,
+        receiver: receiver,
+      }),
+    });
+  };
 
-      // console.log(username)
-      let SearchItems = (searchValue, friends) => {
-        setSearchInput(searchValue)
-        let update
-        let test = [...friends]
-        update = username.filter((item, index) => item.id !== user.user_id && item.id !== (test.length - 1 < index ? null : test[index].friends[0]))
-        setUsername(update)
-        // console.log(test[0].friends[0])
-        // console.log(test)
-        if (searchInput !== '') {
-          let filteredData = username.filter((item) => {
-            return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+  return (
+    <div className="">
+      <Link to="/">Home</Link>
+      <span> | </span>
+
+      {user ? (
+        <div>
+          <div>
+            <Link onClick={logoutUser}>Logout</Link>
+            <span> | </span>
+            <Link to="/chatroomcreator">Create new room</Link>
+          </div>
+        </div>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+      {user && <p>Hello {user.username}</p>}
+
+      {data.map((item, index) => (
+        <ul key={index}>
+          <p>Title: </p>
+          <li>
+            <Link to={`chatroom/${item.title}`}>{item.title}</Link>
+          </li>
+          <p>Description: </p>
+          <li>{item.description}</li>
+
+          <br></br>
+        </ul>
+      ))}
+
+      <h3>Search users: </h3>
+      <form>
+        <input
+          type="text"
+          name="usersearch"
+          placeholder="Type searching query"
+          onChange={(e) => SearchItems(e.target.value, friends)}
+        />
+      </form>
+
+      {searchInput.length > 1
+        ? filteredResults.map((item, index) => {
+            {
+              item.id !== user.user_id && item.id !== (friends.length - 1 < index ? null : friends[index].friends[0]) ? (
+                <p key={index}>
+                  {item.username}{" "}
+                  <span>
+                    <button onClick={() => HandleRequest(item.id)}>
+                      ADD FRIEND
+                    </button>
+                  </span>
+                </p>
+              ) : (
+                <p>joł</p>
+              );
+            }
           })
-          setFilteredResults(filteredData)
-        } else {
-          update = username.filter((item, index) => item.id !== user.user_id && item.id !== (test.length - 1 < index ? null : test[index].friends[0]))
-          setUsername(update)
-        }
-      }
+          : username.map((item, index, test=-1) => {
+            //(friends.length - 1 < index ? null : friends[index].friends[0]) || console.log(friends.length - 1 < index ? null : friends[index].friends[0]), item.id !== friends[index > friends.length ? test=0 : index].friends[0]
+            if (
+              item.id !== user.user_id  || console.log(friends[index > friends.length ? test++ : index].friends[0])
+            ) {
+              return (
+                <p key={index}>
+                  {item.username}{" "}
+                  <span>
+                    <button onClick={() => HandleRequest(item.id)}>
+                      ADD FRIEND
+                    </button>
+                  </span>
+                </p>
+              );
+            }
+            else{
+              <p>oaidaw</p>
+            }
+          })}
+      {friendRequest.map((item, index) => {
+        return (
+          <p key={index}>
+            Friend Request from
+            {item.sender}
+            <button onClick={() => acceptRequest(item.sender, item.id)}>
+              ACCEPT
+            </button>
+            <span> | </span>{" "}
+            <button onClick={() => declineRequest(item.id)}>DECLINE</button>
+          </p>
+        );
+      })}
 
-
-
-      let HandleRequest = async (receiver) => {
-        let response = await fetch('http://127.0.0.1:8000/postingRequests/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({'status': 'pending','sender': user.user_id, 'receiver': receiver})
-        });
-      }
-      
-
-
-    return (
-      <div className=''>
-          <Link to='/'>Home</Link>
-          <span> | </span>
-          
-          {user ? (
-            <div>
-              <div>
-              <Link onClick={logoutUser}>Logout</Link>
-              <span> | </span>
-              <Link to='/chatroomcreator'>Create new room</Link>
-              </div>
-            </div>
-
-          ): (
-            <Link to="/login">Login</Link>
-          )}
-          {user && <p>Hello {user.username}</p>}
-          
-          {
-                data.map((item, index)=>(
-                  <ul key={index}>
-                    <p>Title: </p><li>
-                      <Link to={`chatroom/${item.title}`}>{item.title}</Link>
-                    </li>
-                    <p>Description: </p><li>
-                      {item.description}
-                    </li>
-
-                    <br></br>
-                  </ul>
-
-                ))
-              }
-
-            <h3>Search users: </h3>
-            <form>
-              <input type='text' name='usersearch' placeholder='Type searching query' onChange={(e) => SearchItems(e.target.value, friends)}/>
-            </form>
-             
-                {searchInput.length > 1 ? (
-                    filteredResults.map((item, index) => {
-                    return(
-                      <p key={index}>
-                      {item.username} <span><button onClick={() => HandleRequest(item.id)}>ADD FRIEND</button></span>
-                    </p>
-                    )
-
-                    })
-                ) : (
-                    username.map((item, index) => {
-                        return (
-                            <p key={index}>
-                              {item.username} <span><button onClick={() => HandleRequest(item.id)}>ADD FRIEND</button></span>
-                            </p>
-                        )
-                    })
-                )}
-                {
-               friendRequest.map((item, index) => {
-                    return (
-                      <p key={index}>
-                        Friend Request from
-                        {item.sender}
-                        
-                        <button onClick={() => acceptRequest(item.sender, item.id)}>ACCEPT</button><span> | </span> <button onClick={() => declineRequest(item.id)}>DECLINE</button>
-                      </p>
-                    )
-                  })
-                }
-
-                {
-                  friends.map((item, index) =>{
-                    return(
-                      <p key={index}>
-                        {item.friends}
-                      </p>
-                    )
-                  })
-                }
-                    
-
-      </div>
-    )
+      {friends.map((item, index) => {
+        return <p key={index}>{item.friends}</p>;
+      })}
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
